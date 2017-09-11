f534c8b944fd39cc76b405c71ab2f31904034139
Blake Johnson <blakejohnson6@mail.weber.edu>
2017-09-11 09:37:23 -0600

    init commit

--- Assignment1/Global.asax.cs
+++ Assignment1/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using Assignment1.Models;
 
 namespace Assignment1
 {
@@ -16,6 +18,7 @@ namespace Assignment1
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<BasicSchoolDbContext>(null); //reference to class in BasicSchool model
         }
     }
 }
---
+++ Assignment1/Models/BasicSchool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment1.Models
+{
+    public class BasicSchool
+    {
+    }
+
+    [Table("Course")] //tells entityframework from looking for wrong table
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        public string courseNumber { get; set; }
+        public string courseName { get; set; }
+        public int creditHours { get; set; }
+        public int? maxEnrollment { get; set; }
+    }
+
+    [Table("Section")]
+    public class Section
+    {
+        [Key]
+        public int Id { get; set; }
+        public int sectionId { get; set; }
+        public int sectionNumber { get; set; }
+        public string courseNumber { get; set; }
+        public string sectionDays { get; set; }
+        public DateTime sectionTime { get; set; }
+    }
+
+    public class BasicSchoolDbContext : DbContext
+    {
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Section> sections { get; set; }
+    }
+
+
+
+}
